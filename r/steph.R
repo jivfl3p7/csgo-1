485735a5dffe52e417957014b1f847c5f011530a
wessonmo <wessonmo@gmail.com>
2017-07-13 11:02:03 -0500

    iterate weeks, train/test for logloss calc

--- r/steph.R
+++ r/steph.R
@@ -1,12 +1,12 @@
 # sink("diagnostics/steph.txt")
 
-library("RPostgreSQL")
-library("dplyr")
-library("PlayerRatings")
+library('RPostgreSQL')
+library('dplyr')
+library('PlayerRatings')
 
-drv <- dbDriver("PostgreSQL")
+drv <- dbDriver('PostgreSQL')
 
-con <- dbConnect(drv, user="postgres", password="", host="localhost", port=5432, dbname="esports")
+con <- dbConnect(drv, user='postgres', password='', host='localhost', port=5432, dbname='esports')
 
 query <- dbSendQuery(con, "
   SELECT DISTINCT
@@ -46,24 +46,69 @@ attach(all_data)
 
 head(all_data)
 
+
+# map_data = all_data[(all_data$year == 2017) & (all_data$map_name == 'Cache'),]
+# 
+# train = map_data[map_data$week < 18,c('week','team1_href','team2_href','result')]
+# test = map_data[map_data$week == 18,c('week','team1_href','team2_href','result')]
+# train_adv = map_data$map_pick[map_data$week < 18]
+# test_adv = map_data$map_pick[map_data$week == 18]
+# 
+# robj <- steph(train, gamma = train_adv)
+# 
+# pvals <- predict(robj, test, tng = 3, gamma = test_adv)
+# 
+# test$result
+# pvals
+
 ratings = data.frame()
+logloss_calc = data.frame()
 for (yr in unique(all_data$year)){
   for (map_nm in unique(all_data$map_name[all_data$year == yr])){
-    map_data = all_data[(all_data$year == yr) & (all_data$map_name == map_nm),]
-    
-    x = map_data[,c("week","team1_href","team2_href","result")]
-    p1_map_pick = map_data$map_pick
-    
-    robj <- steph(x, gamma = p1_map_pick)
-    robj$ratings$season = yr
-    robj$ratings$week = max(map_data$week)
-    robj$ratings$map_name = map_nm
-    
-    ratings <- rbind(ratings,robj$ratings)
-  }  
+    for (wk in sort(unique(all_data$week[(all_data$year == yr) & (all_data$map_name == map_nm)]))[-1]){
+      map_data = all_data[(all_data$year == yr) & (all_data$map_name == map_nm) & (all_data$week <= wk),]
+      
+      train = map_data[map_data$week < wk,c('week','team1_href','team2_href','result')]
+      train_adv = map_data$map_pick[map_data$week < wk]
+      test = map_data[map_data$week == wk,c('week','team1_href','team2_href','result')]
+      test_adv = map_data$map_pick[map_data$week == wk]
+      
+      robj <- steph(train, gamma = train_adv)
+      robj$ratings$season = yr
+      robj$ratings$week = wk
+      robj$ratings$map_name = map_nm
+      
+      pvals <- predict(robj, test, tng = 5, gamma = test_adv)
+      
+      if (sum(!is.na(pvals)) > 0){
+        results = test[which(!is.na(pvals)),]
+        results$pvals = pvals[which(!is.na(pvals))]
+        results$season = yr
+        results$map_name = map_nm
+        logloss_calc <- rbind(logloss_calc,results)
+      }
+      
+      ratings <- rbind(ratings,robj$ratings[robj$ratings$Games >= 5,])
+    }
+  }
 }
 
-names(ratings)[names(ratings) == "Player"] <- "team_href"
 
 
-dbWriteTable(con,c("csgo","steph_ratings"),ratings, row.names = F, overwrite = T)
+test = logloss_calc[(logloss_calc$season == 2017) & (logloss_calc$map_name == 'Mirage'),]
+test$logloss = -1*(test$result*log(test$pvals) + (1 - test$result)*log(1 - test$pvals))
+test$agg_ll = cummean(test$logloss)
+
+
+
+(data$win*log(w)+(1-data$win)*log(1-w))
+
+ratings[(ratings$season == 2017) & (ratings$map_name == 'Mirage'),]
+
+
+
+
+names(ratings)[names(ratings) == 'Player'] <- 'team_href'
+
+
+dbWriteTable(con,c('csgo','steph_ratings'),ratings, row.names = F, overwrite = T)
