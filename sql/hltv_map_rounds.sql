a32a5b8411b8c961461fcf884a50bbf30cdc36c2
wessonmo <wessonmo@gmail.com>
2017-07-30 23:49:55 -0500

    added map_num, half, and pseudo_rd

--- sql/hltv_map_rounds.sql
+++ sql/hltv_map_rounds.sql
@@ -4,8 +4,13 @@ drop table if exists csgo.hltv_map_rounds;
 
 create table csgo.hltv_map_rounds (
 	match_href	text,
+	map_num		float,
 	map_name	text,
+	half		float,
+	pseudo_rd	float,
+	team1_href	text,
 	team1_side	text,
+	team2_href	text,
 	team2_side	text,
 	result		float
 );
