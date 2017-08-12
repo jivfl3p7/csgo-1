3885b52d40b3607cdae8a42d16349a4144b030e2
wessonmo <wessonmo@gmail.com>
2017-08-12 17:04:07 -0500

    team_id to team_href

--- sql/hltv_team_ranks.sql
+++ sql/hltv_team_ranks.sql
@@ -6,16 +6,12 @@ create table csgo.hltv_team_ranks (
 	date_		date,
 	rank		float,
 	team_name	text,
-	team_id		text,
+	team_href	text,
 	points		float
 );
 
 truncate table csgo.hltv_team_ranks;
 
 copy csgo.hltv_team_ranks from 'C:\Users\wessonmo\Documents\GitHub\csgo\csv\hltv_team_ranks.csv' with delimiter as ',' csv quote as '"';
-
-update csgo.hltv_team_ranks set team_id = substring(team_id from '[0-9]{1,}(?=\/)');
-
-alter table csgo.hltv_team_ranks alter column team_id type int using (team_id::int);
 
 commit;
