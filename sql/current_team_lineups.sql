4d891652839386f7df404bd5be0dc3f94372c91a
wessonmo <wessonmo@gmail.com>
2017-10-17 23:02:00 -0500

    create shorter team name/lineup strings

--- sql/current_team_lineups.sql
+++ sql/current_team_lineups.sql
@@ -3,40 +3,61 @@ begin;
 drop table if exists csgo.current_team_lineups;
 
 create table csgo.current_team_lineups as (
-	select distinct on (a.lineup_id)
+	select distinct
 		a.team_href
 		,a.team_name
+		,a.match_href
+		,a.datetime_utc
 		,a.lineup_id
-		,a.datetime_utc as latest_match
+		,sum(a.new_lineup) OVER (partition by a.team_href order by a.datetime_utc) as rk
 	from (
-		select distinct on (ml.team_href)
-			ml.team_href
-			,tn.team_name
-			,ml.datetime_utc
-			,ml.lineup_id
-		from csgo.match_lineups as ml
-			left join (
-				select distinct *
-				from (
-					select distinct
-						team1_href as team_href
-						,team1_name as team_name
-					from csgo.hltv_match_info
-					union
-					select distinct
-						team2_href as team_href
-						,team2_name as team_name
-					from csgo.hltv_match_info
-					) as a
-				) as tn
-				on ml.team_href = tn.team_href
+		select distinct
+			a.team_href
+			,a.team_name
+			,a.match_href
+			,a.lineup_id
+			,case
+				when lag(a.team_href) over () != team_href then 0
+				when lag(a.lineup_id) over () != lineup_id then 1
+				else 0
+			end as new_lineup
+			,a.datetime_utc
+		from (
+			select distinct
+				ml.team_href
+				,tn.team_name
+				,ml.match_href
+				,ml.datetime_utc
+				,ml.lineup_id
+			from csgo.match_lineups as ml
+				left join (
+					select distinct *
+					from (
+						select distinct
+							team1_href as team_href
+							,team1_name as team_name
+						from csgo.hltv_match_info
+						union
+						select distinct
+							team2_href as team_href
+							,team2_name as team_name
+						from csgo.hltv_match_info
+						) as a
+					) as tn
+					on ml.team_href = tn.team_href
+			order by
+				ml.team_href
+				,ml.datetime_utc
+			) as a
 		order by
-			ml.team_href
-			,ml.datetime_utc desc
+			a.team_href
+			,a.datetime_utc
+			,a.lineup_id
 		) as a
 	order by
-		a.lineup_id
-		,a.datetime_utc desc
+		a.team_href
+		,a.datetime_utc
+		,a.lineup_id
 );
 
 commit;
