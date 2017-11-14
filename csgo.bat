5ef681ed96c8c0f20f807115d395130885764a95
wessonmo <wessonmo@gmail.com>
2017-11-14 13:07:35 -0600

    add psql for active_teams

--- csgo.bat
+++ csgo.bat
@@ -8,7 +8,7 @@ if "%computername%"=="MITCHELL-LAPTOP" (
 	set ana_path=C:
 )
 
-%ana_path%\Anaconda2\python.exe -W ignore %~dp0\python\hltv_scrape.py
+::%ana_path%\Anaconda2\python.exe -W ignore %~dp0\python\hltv_scrape.py
 ::%ana_path%\Anaconda2\python.exe -W ignore %~dp0\python\rar_to_csv.py
 
 for /f %%a in ('psql -U postgres -c "select 1 as result from pg_database where datname='esports'" -t') do set /a check=%%a
@@ -25,6 +25,7 @@ psql -U postgres -d esports -v init_path=%init_path% -qf sql/hltv_map_results.sq
 psql -U postgres -d esports -v init_path=%init_path% -qf sql/hltv_round_results.sql
 psql -U postgres -d esports -v init_path=%init_path% -qf sql/hltv_player_stats.sql
 psql -U postgres -d esports -v init_path=%init_path% -qf sql/hltv_vetos.sql
+psql -U postgres -d esports -v init_path=%init_path% -qf sql/hltv_active_teams.sql
 psql -U postgres -d esports -qf sql/match_lineups.sql
 psql -U postgres -d esports -qf sql/current_team_lineups.sql
 psql -U postgres -d esports -v init_path=%init_path% -qf sql/demo_info.sql
