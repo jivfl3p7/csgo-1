6cd10905cb61eaa29e22980c7b0b271a174940f7
wessonmo <wessonmo@gmail.com>
2017-11-15 22:26:27 -0600

    combine round_types, +map_name:rd_type, upload sql

--- r/me_with_demo_data.R
+++ r/me_with_demo_data.R
@@ -10,66 +10,75 @@ drv <- dbDriver('PostgreSQL')
 con <- dbConnect(drv, user='postgres', password='', host='localhost', port=5432, dbname='esports')
 
 round_query <- dbSendQuery(con, "
-                           select distinct
-                           left(mi.datetime_utc::varchar,4)::int as season
-                           ,mi.datetime_utc
-                           ,mi.event_href
-                           ,dr.match_href
-                           ,dr.map_num
-                           ,di.map_name
-                           ,dr.round
-                           ,dr.t_href
-                           ,tl2.team_name||'_'||(tl2.rk)::text as t_team
-                           ,tl.lineup_id as t_lineup
-                           ,dr.ct_href
-                           ,ctl2.team_name||'_'||(ctl2.rk)::text as ct_team
-                           ,ctl.lineup_id as ct_lineup
-                           ,dr.ct_econ_adv as ct_econ_adv
-                           ,dr.ct_reward_diff
-                           ,dr.defuse as defuse_kits
-                           ,dr.plant
-                           ,dr.winner as winner
-                           from csgo.demo_rounds as dr
-                           left join csgo.demo_info as di
-                           on
-                           dr.match_href = di.match_href
-                           and dr.map_num = di.map_num
-                           left join csgo.hltv_match_info as mi
-                           on dr.match_href = mi.match_href
-                           left join csgo.hltv_events as e
-                           on mi.event_href = e.event_href
-                           left join csgo.match_lineups as tl
-                           on
-                           dr.match_href = tl.match_href
-                           and dr.t_href = tl.team_href
-                           left join csgo.current_team_lineups as tl2
-                           on
-                           tl.lineup_id = tl2.lineup_id
-                           and tl.match_href = tl2.match_href
-                           left join csgo.match_lineups as ctl
-                           on
-                           dr.match_href = ctl.match_href
-                           and dr.ct_href = ctl.team_href
-                           left join csgo.current_team_lineups as ctl2
-                           on
-                           ctl.lineup_id = ctl2.lineup_id
-                           and ctl.match_href = ctl2.match_href
-                           where 
-                           di.map_name is not null
-                           and dr.round <= 30
-                           order by
-                           mi.datetime_utc
-                           ,dr.match_href
-                           ,dr.map_num
-                           ,dr.round
-                           --limit 100                           
-                           ;")
+select distinct
+  left(mi.datetime_utc::varchar,4)::int as season
+  ,mi.datetime_utc
+  ,mi.event_href
+  ,dr.match_href
+  ,dr.map_num
+  ,di.map_name
+  ,dr.round
+  ,dr.t_href
+  ,tl2.team_name||'_'||(tl2.rk)::text as t_team
+  ,tl.lineup_id as t_lineup
+  ,tl3.active as t_active
+  ,dr.ct_href
+  ,ctl2.team_name||'_'||(ctl2.rk)::text as ct_team
+  ,ctl.lineup_id as ct_lineup
+  ,ctl3.active as ct_active
+  ,dr.ct_econ_adv as ct_econ_adv
+  ,dr.ct_reward_diff
+  ,dr.defuse as defuse_kits
+  ,dr.plant
+  ,dr.winner as winner
+from csgo.demo_rounds as dr
+  left join csgo.demo_info as di
+    on
+    dr.match_href = di.match_href
+    and dr.map_num = di.map_num
+  left join csgo.hltv_match_info as mi
+    on dr.match_href = mi.match_href
+  left join csgo.hltv_events as e
+    on mi.event_href = e.event_href
+  left join csgo.match_lineups as tl
+    on
+    dr.match_href = tl.match_href
+    and dr.t_href = tl.team_href
+  left join csgo.current_team_lineups as tl2
+    on
+    tl.lineup_id = tl2.lineup_id
+    and tl.match_href = tl2.match_href
+  left join csgo.hltv_active_teams as tl3
+    on tl.team_href = tl3.team_href    
+  left join csgo.match_lineups as ctl
+    on
+    dr.match_href = ctl.match_href
+    and dr.ct_href = ctl.team_href
+  left join csgo.current_team_lineups as ctl2
+    on
+    ctl.lineup_id = ctl2.lineup_id
+    and ctl.match_href = ctl2.match_href
+  left join csgo.hltv_active_teams as ctl3
+    on ctl.team_href = ctl3.team_href
+where 
+  di.map_name is not null
+  and dr.round <= 30
+order by
+  mi.datetime_utc
+  ,dr.match_href
+  ,dr.map_num
+  ,dr.round
+--limit 100                           
+;")
 
 round_data = fetch(round_query,n=-1)
 
 round_data$map_name = ifelse((round_data$map_name == 'de_inferno') & (round_data$datetime_utc <= '2016-08-28 14:30:00'),'de_inferno_o',round_data$map_name)
 round_data$ct_win = ifelse(round_data$winner == 3,1,0)
 round_data$ct_econ_group = ifelse(round(round_data$ct_econ_adv/5000) <= -4, -4, ifelse(round(round_data$ct_econ_adv/5000) >= 4, 4,round(round_data$ct_econ_adv/5000)))
+round_data$rd_type = ifelse(round_data$round %in% c(1,2), round_data$round, 0)
+
+active_teams = unique(c(str_sub(round_data[round_data$t_active == 1,'t_team'], end = -3),str_sub(round_data[round_data$ct_active == 1,'ct_team'], end = -3)))
 
 attach(round_data)
 
@@ -81,68 +90,83 @@ match_href = as.factor(match_href)
 event_href = as.factor(event_href)
 ct_econ_group = as.factor(ct_econ_group)
 map_name = as.factor(map_name)
+rd_type = as.factor(rd_type)
 
-rounds = list(-1,c(1,16),c(2,17),c(3,18),c(4,19),c(c(5:13),c(20:28)),c(14,29),c(15,30))
-round_types = c('knife', 'pistol', 'rd2', 'rd3', 'rd4', 'main', 'rd14', 'rd15')
+rounds = list(c(1,16),c(2,17),c(c(3:15),c(18:30)))
+round_types = c('pistol', 'rd2', 'main')
+formula = ct_win ~ (1|ct_team) + (1|t_team) + (1|ct_econ_group)
 
 glmer.function <- function(formula, temp_data, map, round_type){
-  model = glmer(formula, data = temp_data, family = binomial, verbose = 2)
+  model = glmer(formula, data = temp_data, family = binomial, verbose = 0)
   
   relgrad <- with(model@optinfo$derivs,solve(Hessian,gradient))
   if (max(abs(relgrad)) > 0.001){
     print('##### converge issue #####')
   }
   
-  # summary(model)
+  print(summary(model))
   eff <- ranef(model, condVar = T)
   
   t = eff$t_team
-  t$map_name = map
-  t$side = 't'
   t$team = rownames(t)
   t$var = sqrt(attr(eff$t_team, "postVar")[1, 1, ])
   t = merge(t, data.frame(table(temp_data$t_team)), by.x = 'team', by.y = 'Var1', all.x = T)
-  colnames(t)[c(2,6)] <- c("int","rounds")
-  t$round_type = round_type
+  colnames(t)[c(2:4)] <- c("t_int","t_var","t_rounds")
   
   ct = eff$ct_team
-  ct$map_name = map
-  ct$side = 'ct'
   ct$team = rownames(ct)
   ct$var = sqrt(attr(eff$ct_team, "postVar")[1, 1, ])
   ct = merge(ct, data.frame(table(temp_data$ct_team)), by.x = 'team', by.y = 'Var1', all.x = T)
-  colnames(ct)[c(2,6)] <- c("int","rounds")
-  ct$round_type = round_type
+  colnames(ct)[c(2:4)] <- c("ct_int","ct_var","ct_rounds")
   
-  return(rbind(t,ct))
+  comb = merge(ct, t, by = 'team', all.x = T)
+  comb$map_name = map
+  comb$round_type = round_type
+  
+  int_val = data.frame(variable = rownames(coef(summary(model))), coef(summary(model)), row.names = NULL)
+  int_val$map_name = map
+  int_val$round_type = round_type
+  
+  return(list(comb,int_val))
 }
 
 team_str = data.frame()
+fix_eff = data.frame()
 
-for (map in c('all',unique(round_data$map_name))){
-  print(map)
-  formula = ct_win ~ (1|ct_team) + (1|t_team) + (1|ct_econ_group) + (1|event_href)
+for (map in c('all','knife',unique(round_data$map_name))){
   
   if (map == 'all'){
     temp_data = round_data[round != -1,]
-    team_str = rbind(team_str,glmer.function(update(formula, ~ . + (1|map_name)), temp_data, NA, 'all'))
+    rtrn = glmer.function(update(formula, ~ . + (1|map_name:rd_type)), temp_data, NA, map)
+    team_str = rbind(team_str,rtrn[[1]])
+    fix_eff = rbind(fix_eff,rtrn[[2]])
+  } else if (map == 'knife'){
+    temp_data = round_data[round == -1,]
+    rtrn = glmer.function(update(formula, ~ . - (1|ct_econ_group)), temp_data, NA, map)
+    team_str = rbind(team_str,rtrn[[1]])
+    fix_eff = rbind(fix_eff,rtrn[[2]])
   } else {
-    for (i in c(1:8)){
+    for (i in c(1:3)){
+      print(c(map, i))
       temp_data = round_data[(map_name == map) & (round %in% rounds[[i]]),]
-      if (i %in% c(1,2)){
-        team_str = rbind(team_str,glmer.function(update(formula, ~ . - (1|ct_econ_group)), temp_data, map, round_types[i]))
+      if (i == 1){
+        rtrn = glmer.function(update(formula, ~ . - (1|ct_econ_group)), temp_data, map, round_types[i])
       } else {
-        team_str = rbind(team_str,glmer.function(formula, temp_data, map, round_types[i]))
+        rtrn = glmer.function(formula, temp_data, map, round_types[i])
       }
+      team_str = rbind(team_str,rtrn[[1]])
+      fix_eff = rbind(fix_eff,rtrn[[2]])
     }
   }
 }
 
 
+team_str$ct_str = 
 
-current_ct = team_str[(team_str$side == 'ct') & (team_str$round_type == 'all') & (str_sub(team_str$team, start = -2) == '_0'),]
+
+current_ct = team_str[(team_str$side == 'ct') & (team_str$round_type == 'all') & (str_sub(team_str$team, start = -2) == '_0') & (str_sub(team_str$team, end = -3) %in% active_teams),]
 # current_ct$str = current_ct$int - current_ct$var
-current_t = team_str[(team_str$side == 't') & (team_str$round_type == 'all') & (str_sub(team_str$team, start = -2) == '_0'),]
+current_t = team_str[(team_str$side == 't') & (team_str$round_type == 'all') & (str_sub(team_str$team, start = -2) == '_0') & (str_sub(team_str$team, end = -3) %in% active_teams),]
 # current_t$str = current_t$int + current_t$var
 
 # head(current_ct[order(-current_ct$str),],25)
@@ -151,7 +175,8 @@ head(current_ct[order(-current_ct$int),],25)
 head(current_t[order(current_t$int),],25)
 
 
-
+hist(team_str[team_str$side == 't','int'])
+hist(team_str[team_str$side == 'ct','int'])
 
 
 
