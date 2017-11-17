6208095e85a5efacebb42ffcce4108eb954e14ce
wessonmo <wessonmo@gmail.com>
2017-11-17 17:02:51 -0600

    initial upload

---
+++ sql/hltv_match_lineups.sql
@@ -0,0 +1,17 @@
+begin;
+
+drop table if exists csgo.hltv_match_lineups;
+
+create table csgo.hltv_match_lineups (
+	match_href	text,
+	map_name	text,
+	team_href	text,
+	lineup		text
+);
+
+truncate table csgo.hltv_match_lineups;
+
+\set full_path '\'' :init_path '\\hltv_match_lineups.csv\''
+copy csgo.hltv_match_lineups from :full_path with delimiter as ',' csv quote as '"';
+
+commit;
